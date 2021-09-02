9c823959335ec239abe38a5975132401cb9c191b
vino <vino0908@outlook.com>
2021-09-02 23:36:25 +0800

    feat: 完成布局

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,7 +5,16 @@ import "../assets/home.scss";
 export const Home = () => {
     return (
         <div className="home-wrap" style={{backgroundImage: `url(${image})`}}>
-
+            <div className="header"></div>
+            <div className="content">
+                <section className="section1"></section>
+                <section className="section2"></section>
+                <section className="section3"></section>
+                <section className="section4"></section>
+                <section className="section5"></section>
+                <section className="section6"></section>
+                <section className="section7"></section>
+            </div>
         </div>
     );
 };
